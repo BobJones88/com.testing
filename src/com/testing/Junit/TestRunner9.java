3fbde7b68d3975db10e6c3553f38958447f0f381
Unknown <bjpaperstacks@gmail.com>
2018-09-20 18:41:20 -0500

    Junit Testing
    Finished TutorialPoint

--- src/com/testing/Junit/TestRunner9.java
+++ src/com/testing/Junit/TestRunner9.java
@@ -6,7 +6,7 @@ import org.junit.runner.notification.Failure;
 
 public class TestRunner9 {
    public static void main(String[] args) {
-      Result result = JUnitCore.runClasses(JunitTestSuite2.class);
+      Result result = JUnitCore.runClasses(PrimeNumberCheckerTest.class);
 
       for (Failure failure : result.getFailures()) {
          System.out.println(failure.toString());
