c64ecd8cefc38f38141bf8d7c04bbcb3b0dda598
Unknown <bjpaperstacks@gmail.com>
2018-09-24 21:37:11 -0500

    Project Euler
    Finding the largest palindrome made from the product of two 3-digit numbers.

---
+++ src/com/testing/ProjectEuler/LargestPalindrome.java
@@ -0,0 +1,56 @@
+package com.testing.ProjectEuler;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Objects;
+
+public class LargestPalindrome {
+
+
+	ArrayList<Integer> palindromes = new ArrayList<Integer>();
+	
+	public int findLargestPalindrome() {
+		findAllPalindromes();
+		Collections.sort(palindromes, Collections.reverseOrder()); ;
+		return palindromes.get(0);
+	}
+	
+	private void findAllPalindromes(){
+		int product = 0;
+		for(int i = 999; i > 90; i--) {
+			for(int j = 999; j > 90; j--) {
+				product = i * j;
+				if(isPalindrome(product)) {
+					palindromes.add(product);
+				}
+			}
+		}
+	}	
+	
+	private boolean isPalindrome(Integer num) {
+		String str = num.toString();
+		CharSequence str1 = new String();
+		CharSequence str2 = new String();
+		
+		str1 = str.subSequence(0, str.length()/2);
+		if(str.length()%2 == 0) {
+			str2 = str.subSequence(str.length()/2, str.length());
+		}
+		else {
+			str2 = str.subSequence(str.length()/2+1, str.length());
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append(str1);
+
+		StringBuilder sb2 = new StringBuilder();	
+		sb2.append(str2);
+		sb2.reverse();
+		
+//		System.out.println("Str1 = " + sb);
+//		System.out.println("Str2 = " + sb2);
+//		System.out.println("Str length = " + str.length());
+		
+		return Objects.equals(sb.toString(), sb2.toString());
+	}
+}
--- src/com/testing/ProjectEuler/Runner.java
+++ src/com/testing/ProjectEuler/Runner.java
@@ -5,10 +5,8 @@ public class Runner {
 		FindingMultiples fm = new FindingMultiples();
 		System.out.println(fm.listTotal(999));
 		
-//		LargestPrimeFactor lpf = new LargestPrimeFactor();
-//		System.out.println(lpf.findLargestPrimeFactor(13195D));
-		
-		LargestPrimeFactor3 lpf3 = new LargestPrimeFactor3();
-		System.out.println(lpf3.findLargestPrimeFactor(1000423D));
+		LargestPalindrome lp = new LargestPalindrome();
+		System.out.println(lp.findLargestPalindrome());
+//		System.out.println(lp.isPalindrome(9885889));
 	}
 }
